ea8034ce0640c51d470ceee6d2d6807079d74969
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ VB/Form1.vb
@@ -0,0 +1,31 @@
+﻿Imports System
+Imports System.Collections.Generic
+Imports System.ComponentModel
+Imports System.Data
+Imports System.Drawing
+Imports System.Text
+Imports System.Windows.Forms
+Imports DevExpress.XtraScheduler
+
+Namespace WindowsApplication1
+    Partial Public Class Form1
+        Inherits Form
+
+        Public Sub New()
+            InitializeComponent()
+        End Sub
+
+        Private Sub schedulerControl1_AppointmentDrop(ByVal sender As Object, ByVal e As DevExpress.XtraScheduler.AppointmentDragEventArgs) Handles schedulerControl1.AppointmentDrop
+            If IsIntersectingWithAnotherAppointment(e.EditedAppointment,e.NewAppointmentResourceIds) Then
+            If MessageBox.Show("Drop Appointment","test",MessageBoxButtons.YesNo) = System.Windows.Forms.DialogResult.No Then
+                e.Allow = False
+            End If
+            End If
+        End Sub
+
+        Private Function IsIntersectingWithAnotherAppointment(ByVal appointment As DevExpress.XtraScheduler.Appointment, ByVal resourceIdCollection As DevExpress.XtraScheduler.ResourceIdCollection) As Boolean
+            Dim abc As AppointmentBaseCollection = schedulerStorage1.GetAppointments(appointment.Start,appointment.End)
+            Return If(abc.Count>0, True, False)
+        End Function
+    End Class
+End Namespace
---
+++ VB/My Project/AssemblyInfo.vb
@@ -0,0 +1,33 @@
+﻿Imports System.Reflection
+Imports System.Runtime.CompilerServices
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+<Assembly: AssemblyTitle("WindowsApplication1")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyConfiguration("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("WindowsApplication1")>
+<Assembly: AssemblyCopyright("Copyright ©  2009")>
+<Assembly: AssemblyTrademark("")>
+<Assembly: AssemblyCulture("")>
+
+' Setting ComVisible to false makes the types in this assembly not visible 
+' to COM components.  If you need to access a type in this assembly from 
+' COM, set the ComVisible attribute to true on that type.
+<Assembly: ComVisible(False)>
+
+' The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("4f7cd0d5-311f-40c0-8d69-30f8ada7ef6a")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
